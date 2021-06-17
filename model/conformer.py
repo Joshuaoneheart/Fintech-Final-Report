64c0451f7909ac8302eeb4984ea1f19b00342fe5
Joshuaoneheart <newheart0561@gmail.com>
2021-06-18 02:17:11 +0800

    Fix the bug of bos token for all model

--- model/conformer.py
+++ model/conformer.py
@@ -26,12 +26,14 @@ class Seq_Encode(nn.Module):
     encoder_out = self.enc_2_dec(encoder_out)
     tgtmask = (torch.triu(torch.ones(self.config["max_embedding_len"], self.config["max_embedding_len"])) == 1).transpose(0, 1)
     tgtmask = tgtmask.float().masked_fill(tgtmask == 0, float("-inf")).masked_fill(tgtmask == 1, float(0.0)).to(device)
-    outputs = self.decoder(labels.permute(1, 0, 2) , memory = encoder_out, tgt_mask = tgtmask, tgt_key_padding_mask=tgt_padding_mask)
+    start_ = torch.zeros(1, 1, 768).to(device)
+    outputs = self.decoder(torch.cat([start_, labels.permute(1, 0, 2)[:-1, :, :]], dim = 0) , memory = encoder_out, tgt_mask = tgtmask, tgt_key_padding_mask=tgt_padding_mask)
     schedule = (torch.rand(labels.shape[1], labels.shape[0]) >= steps / 5000)
+    outputs = torch.cat([start_, outputs],dim = 0)[:-1, :, :]
     for idx, batch in enumerate(outputs):
         for idx_ in range(len(batch)):
-            if schedule[idx, idx_].item() or idx == 0:
-                outputs[idx,idx_,:] = labels[idx_, idx, :]
+            if schedule[idx, idx_].item() and idx + 1 < self.config["max_embedding_len"]:
+                outputs[idx + 1,idx_,:] = labels[idx_, idx, :]
     outputs = self.decoder(outputs.detach() , memory = encoder_out, tgt_mask = tgtmask, tgt_key_padding_mask = tgt_padding_mask)
     return outputs
 
@@ -45,13 +47,13 @@ class Seq_Encode(nn.Module):
     out = self.prenet(batch)
     encoder_out, _ = self.encoder(out, out.size(1))
     encoder_out = self.enc_2_dec(encoder_out)
-    start_ = labels[:, 0, :]
     outputs = torch.zeros(length[0], 1, 768).float().to(device)
-    outputs[0, :, :] = start_.unsqueeze(0).permute(1, 0, 2)
-    for i in range(1, length[0]):
+    start_ = torch.zeros(1, 1, 768).to(device)
+    outputs[0, :, :] = start_
+    for i in range(1, length[0] + 1):
         tgtmask = (torch.triu(torch.ones(i, i)) == 1).transpose(0, 1)
         tgtmask = tgtmask.float().masked_fill(tgtmask == 0, float("-inf")).masked_fill(tgtmask == 1, float(0.0)).to(device)
         tmp_out = self.decoder(outputs[:i, :, :] , memory = encoder_out, tgt_mask = tgtmask, tgt_key_padding_mask = tgt_padding_mask[:, :i])
         outputs[i, :, :] = tmp_out[-1, :, :]
         
-    return outputs
+    return outputs[1:, :, :]
